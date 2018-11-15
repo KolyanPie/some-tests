511369f6649cea51d396ac57d5d3c2569cfee0a4
Michael <43596340+KolyanPie@users.noreply.github.com>
2018-11-15 07:49:15 +0300

    add method read & write

--- src/ru/edu/bm/package1/Main.java
+++ src/ru/edu/bm/package1/Main.java
@@ -1,7 +1,35 @@
 package ru.edu.bm.package1;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
         System.err.println("Have you a trouble? Double trouble may be?");
+    }
+
+    private void write(Socket socket, String query) {
+        try {
+
+            OutputStream out = socket.getOutputStream();
+            out.write(query.getBytes());
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String read(Socket socket) {
+        try {
+            InputStream in = socket.getInputStream();
+            byte [] buf = new byte [32 * 1024];
+            int readBytes = in.read(buf);
+            return new String(buf, 0, readBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
