b8b7f049892495501af4e3d61ecba4bbe10e5198
Michael <43596340+KolyanPie@users.noreply.github.com>
2018-11-29 19:14:28 +0300

    little changes for SD

--- src/SD/DigitFilter.java
+++ src/SD/DigitFilter.java
@@ -1,37 +1,26 @@
 package SD;
 
-import javax.swing.*;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
 
 public class DigitFilter extends DocumentFilter {
     private static final String DIGITS = "\\d+";
-    private JTextField textField;
-
-    DigitFilter(JTextField textField) {
-        this.textField = textField;
-    }
 
     @Override
     public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr)
             throws BadLocationException {
 
-        if (string.matches(DIGITS) && (textField.getText().length() + string.length() < 5)) {
+        if (string.matches(DIGITS)) {
             super.insertString(fb, offset, string, attr);
-            System.out.println(1);
         }
     }
 
     @Override
     public void replace(FilterBypass fb, int offset, int length, String string, AttributeSet attrs)
             throws BadLocationException {
-        int l = 0;
-        if (textField.getSelectedText() != null) {
-            l = textField.getSelectedText().length();
-        }
 
-        if (string.matches(DIGITS) && (textField.getText().length() + string.length() - l < 6)) {
+        if (string.matches(DIGITS)) {
             super.replace(fb, offset, length, string, attrs);
         }
     }
